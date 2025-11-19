d2754abed4b4f07053bdb821614fc9c37cae77cf
HimanshuRajput <himanshurajput757@gmail.com>
2025-11-19 15:09:34 +0530

    Fix import path typo in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Navbar from '../componets/Navbar/navbar'
-import Trending from '../componets/Trending/Trending'
-import Reasons from '../componets/Reasons/Reasons'
-import Questions from '../componets/Questions/Questions'
-import Subscription from '../componets/Subscription/Subscription'
-import Footer from '../componets/Footer/Footer'
+import Navbar from '../components/Navbar/navbar'
+import Trending from '../components/Trending/Trending'
+import Reasons from '../components/Reasons/Reasons'
+import Questions from '../components/Questions/Questions'
+import Subscription from '../components/Subscription/Subscription'
+import Footer from '../components/Footer/Footer'
 
 function Home() {
   return (
